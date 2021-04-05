f06b370384839cf0e98b51c94403a913cc4ef0ca
emmyseco <emmyseco@gmail.com>
2021-04-05 06:26:39 -0500

    ultimos cambios

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -4,18 +4,20 @@
             <h1>Visión general</h1>
             <p>Ingeniero de Sistemas y desarrollador web, me dedico desde hace mucho tiempo en la creación de soluciones web, desde paginas web simples hasta aplicaciones a medida a diversas empresas.</p>
             <p>Cada trabajo realizado se aplica la profesionalidad de mi perfil y la dedicación para lograr los objetivos esperados por los clientes.</p>
-            <v-row>
-                <v-col v-for="n in 6" :key="n" md="2">
-                    <v-card  rounded>
-                        <v-card-text align="center">
-                            <v-img contain max-width="60" max-height="55" :lazy-src="imageSkill[n-1].src" :src="imageSkill[n-1].src"></v-img>
-                        </v-card-text>
-                        <v-card-actions class="text-subtitle-2 justify-center">
-                            <p class="mb-0">{{ imageSkill[n-1].title }}</p>
-                        </v-card-actions>
-                    </v-card>
-                </v-col>
-            </v-row>
+            <VueSlickCarousel v-bind="settingSlickIcons">
+                <div class="row justify-center flexForced" v-for="(group,i) in iconGroup" :key="i">
+                    <div class="col-md-2 col" v-for="(icon,n) in imageSkill.slice(i*imageSkillPerRow,(i+1) * imageSkillPerRow)" :key="n">
+                        <v-card  rounded>
+                            <v-card-text align="center">
+                                <v-img contain max-width="60" max-height="55" :lazy-src="icon.src" :src="icon.src"></v-img>
+                            </v-card-text>
+                            <v-card-actions class="text-subtitle-2 justify-center">
+                                <p class="mb-0">{{ icon.title }}</p>
+                            </v-card-actions>
+                        </v-card>
+                    </div>
+                </div>
+            </VueSlickCarousel>
             <v-row class="my-5">
                 <v-col  >
                     <v-btn elevation="1" rounded color="primary" >
@@ -37,6 +39,7 @@
         min-height: 350px;
         z-index: 1;
     }
+    .flexForced{ display:flex!important; }
     .overview::before{
         content:'';
         position: absolute;
@@ -56,17 +59,41 @@
     .index-1{ z-index: 1; }
 </style>
 <script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css' // optional style for arrows & dots
+
 export default {
+    components: { VueSlickCarousel },
     data(){
         return {
+            imageSkillPerRow: 3,
             imageSkill:[
                 {title:"Javascript", src: "https://api.briantastic.com/assets/icons/Group-1.svg"},
                 {title:"Vue JS", src: "https://cdn.worldvectorlogo.com/logos/vue-js-1.svg"},
-                {title:"C#", src: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/C_Sharp_logo.svg/512px-C_Sharp_logo.svg.png"},
+                {title:"C#", src: "https://seeklogo.com/images/C/c-sharp-c-logo-02F17714BA-seeklogo.com.png"},
                 {title:"HTML5", src: "https://api.briantastic.com/assets/icons/HTML5_logo_and_wordmark.svg"},
                 {title:"PHP", src: "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg"},
                 {title:"MySQL", src: "https://www.vectorlogo.zone/logos/mysql/mysql-ar21.svg"},
             ],
+            settingSlickIcons:{
+                "dots": true,
+                "focusOnSelect": true,
+                "infinite": true,
+                "speed": 500,
+                "slidesToShow": 1,
+                "slidesToScroll": 1,
+                "autoplay": true,
+                "autoplaySpeed": 2000,
+                "cssEase": "linear",
+                // "touchThreshold": 5
+                
+            }
+        }
+    },
+    computed:{
+        iconGroup(){
+            return Array.from(Array(Math.ceil(this.imageSkill.length / this.imageSkillPerRow)).keys())
         }
     }
 }
